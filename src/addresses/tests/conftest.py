aba395b6aec3ed05ef012dde246a48807cdc5720
Jakub Jadczak <jakubjadczak02@aoutlook.com>
2024-04-23 16:05:10 +0200

    [GHI#19], feat: Setup postgreSQL

--- src/addresses/tests/conftest.py
+++ src/addresses/tests/conftest.py
@@ -26,8 +26,12 @@ def pytest_configure():
             ],
             DATABASES={
                 "default": {
-                    "ENGINE": "django.db.backends.sqlite3",
-                    "NAME": BASE_DIR / "db.sqlite3",
+                    "ENGINE": "django.db.backends.postgresql_psycopg2",
+                    "NAME": "postgres",
+                    "USER": "postgres",
+                    "PASSWORD": "postgres",
+                    "HOST": "localhost",
+                    "PORT": 5432,
                 }
             },
             # Wszystkie inne potrzebne ustawienia
